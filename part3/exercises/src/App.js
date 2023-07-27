65fb69d0af166155716c35691e46efd10b4970f2
may2fn <may2fn@umsystem.edu>
2023-07-27 11:34:32 -0500

    Added exercise to part3

---
+++ part3/exercises/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import HobbyIntroduction from './components/Introduction';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My Hobby: Bowling</h1>
+      <HobbyIntroduction />
+    </div>
+  );
+}
+
+export default App;
---
+++ part3/exercises/src/components/Introduction.js
@@ -0,0 +1,12 @@
+export default function HobbyIntroduction(){
+    return (
+        <div>
+            <h2>3 Fun Facts About My Hobby</h2>
+            <ol>
+                <li>On 1 January 1840, Knickerbocker Alleys in New York City opened, becoming the first indoor bowling alley</li>
+                <li>Nine-pins was the most popular form of bowling in much of the United States from colonial times until the 1830s.</li>
+                <li>A bowling lane is 18.2 meters (around 60 feet) long from the foul line to the headpin.</li>
+            </ol>
+        </div>
+    );
+}
